25f9a4f3e3fe02fb7f99ade8dc19d52d0e721d67
Viktoria <vikyskamalyshzp@gmail.com>
2022-05-05 01:03:50 +0300

    04.05

--- front-montana/src/pages/about/Comments.js
+++ front-montana/src/pages/about/Comments.js
@@ -1,6 +1,9 @@
 import { Segment } from "semantic-ui-react";
 import "./comments.css";
-
+import "font-awesome/css/font-awesome.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
+import { faHome, quoteRight } from "@fortawesome/free-solid-svg-icons";
 const comments = [
   {
     id: 1,
@@ -66,11 +69,23 @@ function Comments() {
         It is a long established fact that a reader will be distracted by the
         readable content of a page when looking at its layout.
       </span>
+      <FontAwesomeIcon icon="fa-brands fa-twitter" />
+
       <div className="div-ab-comments-block">
         {comments.map((item) => (
           <div className="div-ab-comments-item">
-            {item.userName}
-            <i className="fa fa-quote-right" ></i>
+            {/* {item.userName} */}
+
+            <span className="bnm">
+              <i className="fa fa-quote-right"></i>
+            </span>
+            <div className="div-ab-comments-item-title">
+            <span className="bnm1">
+              <i class="fa fa-star"></i>
+            </span>
+              <span className="span-ab-comment">{item.comment}</span>
+
+            </div>
           </div>
         ))}
       </div>
